2905d0e12ad71cd538a38c488c03abf837781ab7
Kevin Lapointe <156107790+lapointek@users.noreply.github.com>
2025-02-05 16:59:32 -0500

    added pages

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,24 @@
+import Navbar from "./components/Navbar";
+import { Routes, Route } from "react-router-dom";
+import HomePage from "./pages/HomePage";
+import SettingsPage from "./pages/SettingsPage";
+import ProfilePage from "./pages/ProfilePage";
+import LoginPage from "./pages/LoginPage";
+import SignupPage from "./pages/SignupPage";
+
 const App = () => {
-    return <div className="text-red-500">Hello World</div>;
+    return (
+        <div>
+            <Navbar />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/signup" element={<SignupPage />} />
+                <Route path="/login" element={<LoginPage />} />
+                <Route path="/settings" element={<SettingsPage />} />
+                <Route path="/profile" element={<ProfilePage />} />
+            </Routes>
+        </div>
+    );
 };
 
 export default App;
