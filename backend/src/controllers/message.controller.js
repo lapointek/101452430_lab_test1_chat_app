09edc0d507fb6c35c3ecb14339ccbb6ef3483cec
Kevin Lapointe <156107790+lapointek@users.noreply.github.com>
2025-02-05 16:07:23 -0500

    added sendMessage function

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,4 +1,5 @@
 import User from "../models/user.model.js";
+import Message from "../models/message.model.js";
 
 export const getUsersForSidebar = async (req, res) => {
     try {
@@ -9,6 +10,45 @@ export const getUsersForSidebar = async (req, res) => {
         res.status(200).json(filteredUsers);
     } catch (error) {
         console.log("error", error.message);
+        res.status(500).json({ error: "Internal server error" });
+    }
+};
+
+export const getMessages = async (req, res) => {
+    try {
+        const { id: userToChatId } = req.params;
+        const myId = req.user._id;
+
+        const messages = await Message.find({
+            $or: [
+                { senderId: myId, receiverId: userToChatId },
+                { senderId: userToChatId, recieverId: myId },
+            ],
+        });
+
+        res.status(200).json(messages);
+    } catch (error) {
+        console.log("Error", error.message);
+        res.status(500).json({ error: "Internal server error" });
+    }
+};
+
+export const sendMessage = async (req, res) => {
+    try {
+        const { text } = req.body;
+        const { id: receiverId } = req.params;
+        const senderId = req.user._id;
+
+        const newMessage = new Message({
+            senderId,
+            recieverId,
+            text,
+        });
+        await newMessage.save();
+        // socket.io
+        res.status(201).json(newMessage);
+    } catch (error) {
+        console.log("Error", error.message);
         res.status(500).json({ error: "Internal server error" });
     }
 };
--- backend/src/routes/message.route.js
+++ backend/src/routes/message.route.js
@@ -1,9 +1,15 @@
 import express from "express";
 import { protectRoute } from "../middleware/auth.middleware.js";
-import { getUsersForSidebar } from "../controllers/message.controller.js";
+import {
+    getMessages,
+    getUsersForSidebar,
+} from "../controllers/message.controller.js";
 
 const router = express.Router();
 
 router.get("/users", protectRoute, getUsersForSidebar);
+router.get("/:id", protectRoute, getMessages);
+
+router.post("/send/:id", protectRoute, sendMessage);
 
 export default router;
