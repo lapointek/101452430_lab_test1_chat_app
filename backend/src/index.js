2f1441a04cafebe49bad199521ced8d734b0b346
Kevin Lapointe <156107790+lapointek@users.noreply.github.com>
2025-02-05 15:11:52 -0500

    added model, .env,controller

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,9 +5,11 @@ import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 const app = express();
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 const PORT = process.env.PORT;
+
 app.listen(PORT, () => {
     console.log(`Server is running of port ${PORT}`);
     connectDB();
