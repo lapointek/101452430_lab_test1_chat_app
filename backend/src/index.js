e002d0c71fda0e245fd2e43dce9e218b5f513a2b
Kevin Lapointe <156107790+lapointek@users.noreply.github.com>
2025-02-05 15:56:24 -0500

    added message model and controller

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,13 +3,16 @@ import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
 
 dotenv.config();
 const app = express();
+
 app.use(express.json());
 app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
+app.use("/api/message", messageRoutes);
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
