1e2c12ab430738eb8f5506e9c4bb06f44e118cde
Kevin Lapointe <156107790+lapointek@users.noreply.github.com>
2025-02-05 15:37:45 -0500

    added middleware

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,13 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
 import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 
 dotenv.config();
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 const PORT = process.env.PORT;
