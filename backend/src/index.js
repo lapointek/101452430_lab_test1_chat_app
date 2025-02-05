fa335ae5f8b49be46f74bff1e77ae3e81ee57f4d
Kevin Lapointe <156107790+lapointek@users.noreply.github.com>
2025-02-05 14:37:48 -0500

    added connection to mongodb in backend

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,14 @@
 import express from "express";
+import dotenv from "dotenv";
+import { connectDB } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
 
+dotenv.config();
 const app = express();
 
 app.use("/api/auth", authRoutes);
-
-app.listen(5001, () => {
-    console.log("Server is running of port 5001");
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+    console.log(`Server is running of port ${PORT}`);
+    connectDB();
 });
